ea2a42a6f5ec3ccca5044edd1258d9fc38fe66da
PhanVinh09 <phanthevinh09w25@gmail.com>
2025-10-31 16:36:11 +0700

    fix lỗi sung đột css của tìm kiếm và bình luận

--- wordpress/wp-content/themes/twentytwenty/template-parts/content.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/content.php
@@ -80,7 +80,7 @@
 	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && ! post_password_required()) {
 	?>
 
-		<div class="comments-wrapper section-inner">
+		<div class="comments-wrapper section-inner-box">
 
 			<?php comments_template(); ?>
 
